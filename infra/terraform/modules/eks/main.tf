288dce994eb246cfe6d72a447656a43f67fe2b04
nivednarayan <nivednarayan09@gmail.com>
2026-05-21 21:54:59 +0530

    Changed k8s version to a version supported by eks

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "main" {
   name = "${var.project_name}-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.29"
+  version  = "1.31"
 
   vpc_config {
     subnet_ids = concat(var.private_subnet_ids, var.public_subnet_ids)
